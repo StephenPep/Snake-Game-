51b4356ba1872c1b90171343a72b5456759304cc
StephenPep <swereko@stetson.edu>
2025-02-24 13:43:13 -0500

    Snake Game.py

--- Snake Game.py
+++ Snake Game.py
@@ -4,11 +4,11 @@ import random
 
 delay = 0.1
 
-# Score
+# Score details 
 score = 0
 high_score = 0
 
-# Set up the screen
+# Settiing up the screen
 wn = turtle.Screen()
 wn.title("Snake Game by Stephen Peprah Wereko")
 wn.bgcolor("green")
@@ -16,7 +16,7 @@ wn.setup(width=600, height=600)
 wn.tracer(0)  # Turns off the screen updates
 
 
-# Snake head
+# Building Snake head
 head = turtle.Turtle()
 head.speed(0)
 head.shape("square")
@@ -45,7 +45,7 @@ pen.hideturtle()
 pen.goto(0, 260)
 pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
 
-# Functions
+
 def go_up():
     if head.direction != "down":
         head.direction = "up"
@@ -79,47 +79,47 @@ def move():
         x = head.xcor()
         head.setx(x + 20)
 
-# Keyboard bindings
+
 wn.listen()
 wn.onkeypress(go_up, "Up")
 wn.onkeypress(go_down, "Down")
 wn.onkeypress(go_left, "Left")
 wn.onkeypress(go_right, "Right")
 
-# Main game loop
+
 while True:
     wn.update()
 
-    # Check for a collision with the border
+    # Checking for a collision with the border
     if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
         time.sleep(1)
         head.goto(0, 0)
         head.direction = "stop"
 
-        # Hide the segments
+       
         for segment in segments:
             segment.goto(1000, 1000)
 
-        # Clear the segments list
+       
         segments.clear()
 
-        # Reset the score
+        # Resetting the score
         score = 0
 
-        # Reset the delay
+        # Resetting the delay
         delay = 0.1
 
         pen.clear()
         pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
 
-    # Check for a collision with the food
+    # Checking for a collision with the food
     if head.distance(food) < 20:
-        # Move the food to a random spot
+        # Moving the food to a random spot
         x = random.randint(-290, 290)
         y = random.randint(-290, 290)
         food.goto(x, y)
 
-        # Add a segment
+       
         new_segment = turtle.Turtle()
         new_segment.speed(0)
         new_segment.shape("square")
@@ -127,10 +127,10 @@ while True:
         new_segment.penup()
         segments.append(new_segment)
 
-        # Shorten the delay
+       
         delay -= 0.001
 
-        # Increase the score
+        # Increasing the score
         score += 10
 
         if score > high_score:
@@ -139,7 +139,7 @@ while True:
         pen.clear()
         pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
 
-    # Move the end segments first in reverse order
+   
     for index in range(len(segments) - 1, 0, -1):
         x = segments[index - 1].xcor()
         y = segments[index - 1].ycor()
